0e5570f30cc251b68b22f5917b3135171182c86d
Noah Franco <noahfranco@jonadabs-mini.lan>
2020-01-31 14:22:52 -0500

    working on ring buffer class

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,7 +8,21 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+    # pseudocode 
+        # if the len of the storgae is qual to the capacity of the linked list  
+        # then we are going to return  
+            # if item is < self.capacity  
+                # self.storage.add_to_tail(item)
+            # else:  
+                # slef.storage.remove_from_head() # assuming overwritten with the newest element measns remove.
+
+        if len(self.storage) == self.capacity:
+            return 
+            if item < self.capacity:
+                self.storage.add_to_tail(item)
+            else:
+                self.storage.remove_from_head()
+                self.current -= 1
 
     def get(self):
         # Note:  This is the only [] allowed
